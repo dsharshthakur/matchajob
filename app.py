00b7d3975323e696ba407fb2096ed682d4c1411d
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-01-15 20:12:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
+
 import os
 import streamlit as st
 import time
