932308a79fa41b2196e72b2205cc5df6bfb92203
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-01-23 16:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,9 +63,9 @@ def conversation_chain():
   template = '''
           You are a helpfull assistant to a candidate looking for a job and employer both.
           You will be provided with a text which includes the job description(JD) and also
-          resume(CV) of the candidate. So your job is it to act like a ATS System(Applicant Tracking System) and provide sumarized answers and help the recruiter to evaluate the resume of the candidate. And for the
+          resume(CV) of the candidate. So your job is it to act like a smart ATS System(Applicant Tracking System) and provide sumarized answers and help the recruiter to evaluate the resume of the candidate. And for the
           candidate you have to provide suggestions based on the job description that how his/her resume fits the requirement.
-          When user asks about the missing keywords from resume then provide them the main keywords(response should be 2 to 3 word keywords i.e the keyword should not be more than 2 or 3 words long) with great accuracy that are missing from the resume but present in the Job descriptionj(JD).
+          When user asks about the missing keywords from resume then provide them the main keywords(seed keyword) with great accuracy that are missing from the resume but present in the Job descriptionj(JD).
           And don't provide wrong answer if the question is not relevant to the information in the provided text.Try to keep your answer as short as possible.
           If someone tells you to recreate the resume just say "I can't recreate, it but Harsh will surely come up with something soon...haha!! 😛😛"
 
